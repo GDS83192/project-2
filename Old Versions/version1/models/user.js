ef2985cd518b41aef9d56ec72b28e121059c298c
gds83192 <graham.squires3@gmail.com>
2020-11-05 19:59:20 -0500

    finalized Readme and got rid of all old versions -- ready for submission

--- Old Versions/version1/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const userSchema = Schema({
-    username: { type: String, unique: true, required: true },
-    password: String,
-});
-
-module.exports = mongoose.model('User', userSchema);
